dd37ac5e201a56c92cafe56a463a1b9e310e7b79
Jonada1 <jonada.ferracaku@nortal.com>
2020-02-20 19:10:16 +0200

    Log out user

--- src/app/diary/diary.page.ts
+++ src/app/diary/diary.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
-import { ToastController } from '@ionic/angular';
+import { Component, OnDestroy } from "@angular/core";
+import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
+import { ToastController } from "@ionic/angular";
 
-import { PhotoService } from '../services/photo.service';
-import { ImagesService } from '../images/images.service';
-import { Observable, Subscription } from 'rxjs';
-import { Image } from '../images/image.model';
-import { apiBase } from '../../environments/urls';
-import { tap } from 'rxjs/operators';
+import { PhotoService } from "../services/photo.service";
+import { ImagesService } from "../images/images.service";
+import { Observable, Subscription } from "rxjs";
+import { Image } from "../images/image.model";
+import { apiBase } from "../../environments/urls";
+import { tap } from "rxjs/operators";
 function dataURLtoFile(dataurl, filename) {
-  const arr = dataurl.split(',');
+  const arr = dataurl.split(",");
   const mime = arr[0].match(/:(.*?);/)[1];
   const bstr = atob(arr[1]);
   let n = bstr.length;
@@ -22,9 +22,9 @@ function dataURLtoFile(dataurl, filename) {
   return new File([u8arr], filename, { type: mime });
 }
 @Component({
-  selector: 'app-diary',
-  templateUrl: 'diary.page.html',
-  styleUrls: ['diary.page.scss']
+  selector: "app-diary",
+  templateUrl: "diary.page.html",
+  styleUrls: ["diary.page.scss"]
 })
 export class DiaryPage implements OnDestroy {
   apiBase = apiBase;
@@ -45,17 +45,23 @@ export class DiaryPage implements OnDestroy {
   saveImage() {
     if (this.currentImage) {
       this.loading = true;
-      try {
-        this.uploadSubscription = this.imageService
-          .upload(dataURLtoFile(this.currentImage, 'image.jpg'))
-          .subscribe((analyzedImage) => {
+      this.uploadSubscription = this.imageService
+        .upload(dataURLtoFile(this.currentImage, "image.jpg"))
+        .subscribe(
+          analyzedImage => {
             this.analyzedImage = analyzedImage;
             this.loading = false;
             this.currentImage = null;
-          });
-      } catch {
-        this.loading = false;
-      }
+          },
+          () => {
+            this.loading = false;
+             this.toastController.create({
+              message: 'Could not start the server',
+              duration: 2000,
+              position: "top"
+            }).then(toast => toast.present);
+          }
+        );
     }
   }
 
@@ -66,17 +72,20 @@ export class DiaryPage implements OnDestroy {
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     };
+    this.loading = true;
 
     this.camera.getPicture(options).then(
       imageData => {
-        this.currentImage = 'data:image/jpeg;base64,' + imageData;
+        this.currentImage = "data:image/jpeg;base64," + imageData;
+        this.loading = false;
       },
       async err => {
+        this.loading = false;
         // Handle error
         const toast = await this.toastController.create({
           message: err,
           duration: 2000,
-          position: 'top'
+          position: "top"
         });
         toast.present();
       }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { UserService } from '../user/user.service';
+import { Component, Input } from "@angular/core";
+import { NavController } from "@ionic/angular";
+import { Storage } from "@ionic/storage";
+import { UserService } from "../user/user.service";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss'],
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.scss"]
 })
 export class NavigationComponent {
-  @Input() route = 'Home';
+  @Input() route = "Home";
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private storage: Storage,
+    private nav: NavController
+  ) {}
   user$ = this.userService.getUser();
 
+  logOut() {
+    this.storage.remove("id_token");
+    this.nav.navigateRoot([`/login`]);
+  }
 }
